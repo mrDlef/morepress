5e224d17880ff5ba0ac37ea4924359bf54894e79
Denis Favreau <dfavreau@and-digital.fr>
2015-04-30 18:19:55 +0200

    Add Post class to Morepress

---
+++ lib/Morepress/Post.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Morepress;
+
+class Post {
+
+	protected $_post;
+
+	public function __construct($post)
+	{
+		is_int($post) and $post = get_post($post);
+		$this->_post = $post;
+	}
+
+	public function addMeta($meta_key, $meta_value, $unique = false)
+	{
+		return add_post_meta($this->_post->ID, $meta_key, $meta_value, $unique);
+	}
+
+	public function updateMeta($meta_key, $meta_value, $prev_value = '')
+	{
+		return update_post_meta($this->_post->ID, $meta_key, $meta_value, $prev_value);
+	}
+
+	public function getMeta($key = '', $single = false)
+	{
+		return get_post_meta($this->_post->ID, $key, $single);
+	}
+
+	public function deleteMeta($meta_key, $meta_value = '') {
+		return delete_post_meta($this->_post->ID, $meta_key, $meta_value);
+	}
+
+}

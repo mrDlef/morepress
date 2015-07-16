404379e01c50baccf6543b3c448f3fec7272ce15
Denis Favreau <dfavreau@and-digital.fr>
2015-07-16 14:43:11 +0200

    Bump version

--- morepress.php
+++ morepress.php
@@ -2,7 +2,7 @@
 
 /*
   Plugin Name: Morepress
-  Version: 0.1.0
+  Version: 0.3.0
   Plugin URI: https://github.com/daidais/morepress
   Description: Framework to do lots of things programmatically with pleasure
   Author: Denis Favreau

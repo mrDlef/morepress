e51cf186125d25fc565cafaffec1202825dc644b
Denis Favreau <dfavreau@and-digital.fr>
2015-08-07 17:17:58 +0200

    Add method add and remove to Menu

--- lib/Morepress/Admin/Menu.php
+++ lib/Morepress/Admin/Menu.php
@@ -5,6 +5,8 @@ namespace Morepress\Admin;
 class Menu
 {
 	protected static $_onadminmenu_registered = false;
+	protected static $_menu_to_add = array();
+	protected static $_menu_to_remove = array();
 	protected static $_submenu_to_add = array();
 	protected static $_submenu_to_remove = array();
 
@@ -16,6 +18,29 @@ class Menu
 			static::$_onadminmenu_registered = true;
 		}
 	}
+
+	public static function add($page_title, $menu_title, $capability, $menu_slug, $function = '', $icon_url = '', $position = null)
+	{
+		static::_register_on_admin_menu();
+		static::$_menu_to_add[] = array(
+			'page_title' => $page_title,
+			'menu_title' => $menu_title,
+			'capability' => $capability,
+			'menu_slug' => $menu_slug,
+			'function' => $function,
+			'icon_url' => $icon_url,
+			'position' => $position,
+		);
+	}
+
+	public static function remove($menu_slug)
+	{
+		static::_register_on_admin_menu();
+		static::$_menu_to_remove[] = array(
+			'menu_slug' => $menu_slug,
+		);
+	}
+
 
 	public static function addSub($parent_slug, $page_title, $menu_title, $capability, $menu_slug, $function = '')
 	{
@@ -41,6 +66,22 @@ class Menu
 
 	public static function onAdminMenu()
 	{
+		if(! empty(static::$_menu_to_add))
+		{
+			foreach(static::$_menu_to_add as $item)
+			{
+				add_menu_page($item['page_title'], $item['menu_title'], $item['capability'], $item['menu_slug'], $item['function'], $item['icon_url'], $item['position']);
+			}
+		}
+
+		if(! empty(static::$_menu_to_remove))
+		{
+			foreach(static::$_menu_to_remove as $item)
+			{
+				remove_menu_page($item['menu_slug']);
+			}
+		}
+
 		if(! empty(static::$_submenu_to_add))
 		{
 			foreach(static::$_submenu_to_add as $item)
@@ -59,3 +100,4 @@ class Menu
 	}
 
 }
+

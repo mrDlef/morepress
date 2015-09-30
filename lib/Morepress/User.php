9669656da1db47c86ca420b80a97ec1b38c445de
Denis Favreau <dfavreau@and-digital.fr>
2015-09-30 12:45:01 +0200

    Improve User class

--- lib/Morepress/User.php
+++ lib/Morepress/User.php
@@ -9,7 +9,20 @@ class User {
 
     protected $_user;
 
-    public static function forge($user) {
+    public static function forge($user = null, $cache = true) {
+        global $wpdb;
+        empty($user) and $user = get_current_user_id();
+
+        if (!$cache and is_numeric($user)) {
+            if (!$user = $wpdb->get_row($wpdb->prepare("SELECT * FROM $wpdb->users WHERE ID = %s", $user))) {
+                return false;
+            }
+            update_user_caches($user);
+        }
+
+        if (empty($user)) {
+            return false;
+        }
         return new static($user);
     }
 
@@ -37,7 +50,7 @@ class User {
 			return static::$_fieldsets[$name];
 		}
 		static::$_fieldsets[$name] = new \Morepress\User\Fieldset($name, $title);
-		
+
 		if (! static::$_onsave_registered)
 		{
 			add_action('personal_options_update', array(__CLASS__, 'onSave'));
@@ -61,8 +74,21 @@ class User {
 		return $this->_user->{$name};
 	}
 
+	public function getWpObject()
+	{
+		return $this->_user;
+	}
+
     public function getMeta($key = '', $single = false) {
         return get_user_meta($this->_user->ID, $key, $single);
+	}
+
+    public function addMeta($key, $value, $single = false) {
+        return add_user_meta($this->_user->ID, $key, $value, $single);
+	}
+
+    public function updateMeta($key, $value, $prev_value = '') {
+        return update_user_meta($this->_user->ID, $key, $value, $prev_value);
 	}
 
 }

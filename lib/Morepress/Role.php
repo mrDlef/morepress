f688c9592ca49a2dd355ab9278d6376d1f91b52d
Denis Favreau <dfavreau@and-digital.fr>
2015-10-12 16:53:01 +0200

    Fix Role and remove useless class Meta_Box/Field

--- lib/Morepress/Role.php
+++ lib/Morepress/Role.php
@@ -2,52 +2,47 @@
 
 namespace Morepress;
 
-class Role
-{
+class Role {
 
 	protected static $_roles = array();
 	protected $_name;
 	protected $_role;
 
-	public static function add($role, $display_name, $capabilities = array())
-	{
+    public static function add($role, $display_name, $capabilities = array()) {
 		add_role($role, $display_name, $capabilities);
 		return new static($role);
 	}
 
-	public static function remove($role)
-	{
+    public static function remove($role) {
 		remove_role($role);
 	}
 
-	public function forge($role)
-	{
-		if (empty(static::$_roles[$role]))
-		{
+    public static function forge($role) {
+        if (empty(static::$_roles[$role])) {
 			static::$_roles[$role] = new static($role);
 		}
 		return static::$_roles[$role];
 	}
 
-	protected function __construct($role)
-	{
+    protected function __construct($role) {
 		$this->_name = $role;
 		$this->_role = get_role($role);
 	}
 
-	public function get()
-	{
+    public function get() {
 		return $this->_role;
 	}
 
-	public function add_cap($cap, $grant = true)
-	{
-		add_cap($this->_name, $cap, $grant);
+    public function addCap($cap, $grant = true) {
+        $this->_role->add_cap($cap, $grant);
 	}
 
-	public function remove_cap($cap)
-	{
-		remove_cap($this->_name, $cap);
+    public function removeCap($cap) {
+        $this->_role->remove_cap($cap);
+    }
+
+    public function __get($key) {
+        $this->_role->{$key};
 	}
 
 }
